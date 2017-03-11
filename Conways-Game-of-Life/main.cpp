28198030d8fea065d11394e4225780ea72563ceb
Ben Reynolds <benr420@gmail.com>
2017-03-11 14:54:46 +0000

    Include and link SFML

--- Conways-Game-of-Life/main.cpp
+++ Conways-Game-of-Life/main.cpp
@@ -1,7 +1,35 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	const unsigned int kWinWidth = 800;
+	const unsigned int kWinHeight = 600;
+	const char* kWinTitle = "Conway's Game of Life";
+
+	sf::RenderWindow window(sf::VideoMode(kWinWidth, kWinHeight), kWinTitle, sf::Style::Close);
+
+	sf::Clock delta_clock;
+	float delta_time = 0.0f;
+
+	while(window.isOpen())
+	{
+		sf::Event event;
+		while(window.pollEvent(event))
+		{
+			switch(event.type)
+			{
+				case sf::Event::Closed:
+					window.close();
+					break;
+			}
+
+			window.clear();
+
+			window.display();
+
+			delta_time = delta_clock.restart().asSeconds();
+		}		
+	}
+
 	return 0;
 }
