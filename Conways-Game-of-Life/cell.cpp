ae3511a396ae6ada633826ad4092a704d4e3b94d
Ben Reynolds <benr420@gmail.com>
2017-03-11 20:56:08 +0000

    Create Cell class

---
+++ Conways-Game-of-Life/cell.cpp
@@ -0,0 +1,39 @@
+#include "cell.h"
+#include <SFML/Graphics/RenderWindow.hpp>
+
+sf::Color const Cell::kClrDead = sf::Color(25, 25, 25);
+sf::Color const Cell::kClrAlive = sf::Color(196, 102, 183);
+sf::Color const Cell::kClrOutline = sf::Color(35, 35, 35);
+
+Cell::Cell() : Cell(sf::Vector2i(0, 0), 32) {}
+
+Cell::Cell(const sf::Vector2i& position, const unsigned int& size)
+{
+	set_alive(false);
+	shape_.setOutlineThickness(1);
+	shape_.setOutlineColor(kClrOutline);
+	shape_.setSize(sf::Vector2f(static_cast<float>(size), static_cast<float>(size)));
+	position_ = position;
+	shape_.setPosition(sf::Vector2f(position.x * shape_.getSize().x, position.y * shape_.getSize().y));
+}
+
+void Cell::draw(sf::RenderWindow& window) const
+{
+	window.draw(shape_);
+}
+
+sf::Vector2i Cell::position() const
+{
+	return position_;
+}
+
+void Cell::set_alive(const bool& alive)
+{
+	alive_ = alive;
+	shape_.setFillColor(alive_ ? kClrAlive : kClrDead);
+}
+
+bool Cell::alive() const
+{
+	return alive_;
+}
---
+++ Conways-Game-of-Life/cell.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <SFML/Graphics/RectangleShape.hpp>
+
+namespace sf {
+	class RenderWindow;
+}
+
+class Cell
+{
+
+ public:
+
+	Cell();
+	explicit Cell(const sf::Vector2i& position, const unsigned int& size);
+
+	void draw(sf::RenderWindow& window) const;
+
+	sf::Vector2i position() const;
+
+	void set_alive(const bool &alive);
+	bool alive() const;
+
+ private:
+
+	static const sf::Color kClrDead;
+	static const sf::Color kClrAlive;
+	static const sf::Color kClrOutline;
+		
+	sf::RectangleShape shape_;
+	sf::Vector2i position_;
+
+	bool alive_;
+
+};
+
